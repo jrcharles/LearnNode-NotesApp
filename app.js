6e69286227589354fcba69791054073d65d53416
play.js <git@gituser.com>
2020-07-03 17:47:38 -0400

    testing more play.js integrations

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
+const validator = require('validator')
 
 const notes = getNotes()
 console.log(notes)
 
 console.log(chalk.green.inverse.bold("Success!"))
+
+console.log(validator.isEmail('jrcharles@mac.com'))
